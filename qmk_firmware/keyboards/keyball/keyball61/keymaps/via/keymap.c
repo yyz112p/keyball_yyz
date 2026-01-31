d219a48b9141d485509265bec1b88635847e323d
yyz112p <o.ryouhei@gmail.com>
2026-01-31 23:22:07 +0900

    first commit

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
@@ -54,15 +54,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______  , _______  , _______  , KC_NO    , KC_NO    , _______  , _______  ,        C(S(KC_TAB)) , C(KC_TAB), _______  , _______  , _______  , _______  , KC_NO    
   ),
 //Fn2
-/*
   [4] = LAYOUT_universal(
-             ,          ,          ,         ,         ,           ,                                           ,          ,          ,          ,           ,          ,
+       KC_NO , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                                   KC_NO   , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,
     _______  , KC_PSLS  , KC_7     , KC_8    , KC_9    , S(KC_MINS),                                   KC_PMNS , KC_7     , KC_8     , KC_9     , S(KC_MINS), _______  ,
-    _______  , KC_PAST  , KC_4     , KC_5    , KC_6    , KC_PMNS   ,                                   KC_PPLS , KC_4     , KC_5     , KC_6     , KC_PSLS   ,          ,
-    _______  , KC_0     , KC_1     , KC_2    , KC_3    , KC_PPLS   ,          ,                      , KC_0    , KC_1     , KC_2     , KC_3     , KC_PAST   ,          ,
-    _______  , _______  , KC_DOT   ,          ,          , _______  , _______  ,        C(S(KC_TAB)) , C(KC_TAB), _______  , _______  , _______  , _______  ,          
+    _______  , KC_PAST  , KC_4     , KC_5    , KC_6    , KC_PMNS   ,                                   KC_PPLS , KC_4     , KC_5     , KC_6     , KC_PSLS   , KC_NO    ,
+    _______  , KC_0     , KC_1     , KC_2    , KC_3    , KC_PPLS   , KC_NO    ,              KC_NO  , KC_0     , KC_1     , KC_2     , KC_3     , KC_PAST   , KC_NO    ,
+    _______  , _______  , KC_DOT   , KC_NO   , KC_NO    , _______  , _______  ,        C(S(KC_TAB)) , C(KC_TAB), _______  , _______  , _______  , _______   , KC_NO         
   ),
-*/
 };
 // clang-format on
 
