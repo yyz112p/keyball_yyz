e29516bb09a515f4e175c2c8c13044887f24b775
yyz112p <o.ryouhei@gmail.com>
2026-01-31 23:15:20 +0900

    first commit

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/keymap.c
@@ -32,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //Fn1
   [1] = LAYOUT_universal(
      KC_NO , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                                   KC_NO   , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,
-    _______, KC_ESC   , S(KC_TAB), KC_UP    , KC_TAB   ,          ,                                  S(KC_4)  , S(KC_7)  , S(KC_2)  , S(KC_6)  , KC_LBRC  , KC_EQL   ,
+    _______, KC_ESC   , S(KC_TAB), KC_UP    , KC_TAB   , KC_NO    ,                                  S(KC_4)  , S(KC_7)  , S(KC_2)  , S(KC_6)  , KC_LBRC  , KC_EQL   ,
     _______, KC_HOME  , KC_LEFT  , KC_DOWN  , KC_RGHT  , KC_END ,                                    S(KC_5)  , S(KC_8)  , S(KC_9)  , KC_QUOT  , KC_SCLN  , S(KC_INT3)  ,
     _______, C(KC_PGUP), KC_F2   , KC_INT5  , KC_INT4  , C(KC_PGDN) , KC_NO  ,              KC_NO  , S(KC_3)  , KC_RBRC  , KC_BSLS  , KC_NO    , S(KC_1)  ,S(KC_INT1),
     _______, KC_NO    , _______  , KC_NO    , _______  , _______  , _______  ,            _______  , _______  , _______  ,S(KC_RGUI), _______  , KC_NO    , _______
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [2] = LAYOUT_universal(
        KC_NO , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                                   KC_NO   , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,
     _______  , _______  , _______  , _______  , _______  , _______  ,                                  CPI_I100 , KC_NO    , SCRL_DVD , KC_NO    , KC_PSCR  , KC_NO    ,
-    _______  , _______  , _______  , _______  , _______  , _______  ,                                  CPI_D100 , KC_BTN1  , MS_BTN3  , KC_BTN2  , KC_NO    , _______  ,
+    _______  , _______  , _______  , _______  , _______  , _______  ,                                  CPI_D100 , KC_BTN1  , KC_BTN3  , KC_BTN2  , KC_NO    , _______  ,
     _______  , _______  , _______  , _______  , _______  , _______  , KC_NO    ,            S(KC_9)  , KBC_SAVE , _______  , SCRL_DVI , _______  , _______  , _______  ,
     _______  , _______  , _______  , KC_NO    , KC_NO    , _______  , _______  ,        C(S(KC_TAB)) , C(KC_TAB), _______  , _______  , _______  , _______  , _______
   ),
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [3] = LAYOUT_universal(
        KC_NO , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,                                   KC_NO   , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    ,
     _______  , KC_VOLU  , KC_F7    , KC_F8    , KC_F9    , KC_F10   ,                                C(S(KC_T)) , KC_BTN4  , KC_PGUP  , KC_BTN5  , KC_PSCR  , _______  ,
-    _______  , KC_VOLD  , KC_F4    , KC_F5    , KC_F6    , KC_F11   ,                                    KC_F5  , KC_BTN1  , KC_BTN3  , KC_BTN2  , KC_NO    ,          ,
+    _______  , KC_VOLD  , KC_F4    , KC_F5    , KC_F6    , KC_F11   ,                                    KC_F5  , KC_BTN1  , KC_BTN3  , KC_BTN2  , KC_NO    , KC_NO    ,
     _______  , KC_NO    , KC_F1    , KC_F2    , KC_F3    , KC_F12   , KC_NO    ,             KC_NO   , A(KC_F4) , C(KC_W)  , KC_PGDN  , KC_INT5  , _______  , _______  ,
     _______  , _______  , _______  , KC_NO    , KC_NO    , _______  , _______  ,        C(S(KC_TAB)) , C(KC_TAB), _______  , _______  , _______  , _______  , KC_NO    
   ),
