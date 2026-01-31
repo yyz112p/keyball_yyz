c98531e0434e3b21301f626a8c281f8f1f369491
yyz112p <o.ryouhei@gmail.com>
2026-01-31 17:05:36 +0900

    first commit

--- qmk_firmware/keyboards/keyball/keyball61/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball61/keymaps/via/config.h
@@ -35,5 +35,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define TAP_CODE_DELAY 5
 
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 2
